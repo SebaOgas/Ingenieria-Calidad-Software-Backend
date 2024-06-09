1bcf32ace2a4aebad3184a8b3cc4c81e8b8e52ce
AndreTati <andrea.duran@alumnos.frm.utn.edu.ar>
2024-06-09 17:01:52 -0300

    correcciones ut

--- src/test/java/utn/ics/repositories/ProductoRepositoryTest.java
+++ src/test/java/utn/ics/repositories/ProductoRepositoryTest.java
@@ -3,14 +3,20 @@ package utn.ics.repositories;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import jakarta.persistence.EntityManager;
+
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.DirtiesContext;
+import utn.ics.entities.Categoria;
 import utn.ics.entities.Marca;
 import utn.ics.entities.Producto;
+import utn.ics.entities.Subcategoria;
 
 @DataJpaTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -22,10 +28,23 @@ class ProductoRepositoryTest {
   @Test
   void testFiltrado() {
     Marca marca = Marca.builder().nombre("ABC").descripcion("Descrip Marca").build();
+    Subcategoria subcategoria = Subcategoria.builder().titulo("Sub").build();
+    List<Subcategoria> subcategorias=new ArrayList<>();
+    subcategorias.add(subcategoria);
+    Categoria categoria = Categoria.builder().titulo("Categoria").subcategorias(subcategorias).build();
 
     entityManager.persist(marca);
     entityManager.flush();
     marca = entityManager.find(Marca.class, 1);
+
+    entityManager.persist(categoria);
+    entityManager.flush();
+    categoria = entityManager.find(Categoria.class, 1);
+
+    subcategoria = categoria.getSubcategorias().iterator().next();
+    entityManager.persist(subcategoria);
+    entityManager.flush();
+    subcategoria = entityManager.find(Subcategoria.class, 1);
 
     Producto producto1 =
         Producto.builder()
@@ -33,6 +52,7 @@ class ProductoRepositoryTest {
             .descripcion("Descrip Producto")
             .visibilidad(false)
             .marca(marca)
+            .subcategoria(subcategoria)
             .build();
 
     entityManager.persist(producto1);
@@ -44,6 +64,7 @@ class ProductoRepositoryTest {
             .descripcion("Descrip Producto")
             .visibilidad(true)
             .marca(marca)
+                .subcategoria(subcategoria)
             .build();
 
     Calendar calendar = Calendar.getInstance();
