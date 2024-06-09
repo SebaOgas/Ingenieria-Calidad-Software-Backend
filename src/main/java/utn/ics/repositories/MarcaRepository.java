05ad9f82bbcdfbdb429faca4d8853df31e00b6b5
LuccaB13 <Lucca.13@outlook.com>
2024-06-09 19:49:05 -0300

    Mis 3 test

--- src/main/java/utn/ics/repositories/MarcaRepository.java
+++ src/main/java/utn/ics/repositories/MarcaRepository.java
@@ -1,6 +1,9 @@
 package utn.ics.repositories;
 
+import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import utn.ics.entities.Marca;
 
@@ -8,4 +11,6 @@ import utn.ics.entities.Marca;
 public interface MarcaRepository extends BaseRepository<Marca, Long> {
 
   Optional<Marca> findByNombre(String nombre);
+  @Query("SELECT p.marca FROM Producto p WHERE p.nombre = :productoNombre")
+  Optional<Marca> findMarcaByNombreProducto(String productoNombre);
 }
---
+++ src/test/java/utn/ics/controllers/MarcaControllerTest.java
@@ -0,0 +1,76 @@
+package utn.ics.controllers;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import utn.ics.IcsApplication;
+import utn.ics.entities.Marca;
+import utn.ics.services.MarcaServiceImpl;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest(classes = IcsApplication.class)
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+
+public class MarcaControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private MarcaServiceImpl marcaService;
+    @InjectMocks
+    private MarcaController marcaController;
+
+    @Test
+    void testCreateMarca() throws Exception {
+        Marca marca = Marca.builder()
+                .nombre("Nike")
+                .descripcion("Descripción de nike?????")
+                .build();
+
+
+        when(marcaService.save(any(Marca.class))).thenReturn(marca);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/marca")
+                        .content(new ObjectMapper().writeValueAsString(marca))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nombre", is("Nike")))
+                .andExpect(jsonPath("$.descripcion", is("Descripción de nike?????")));
+    }
+    @Test
+    void testGetAllMarcas()throws Exception{
+        Marca marca1 = Marca.builder().nombre("Marca 1").build();
+        Marca marca2 = Marca.builder().nombre("Marca 2").build();
+        List<Marca> marcas = List.of(marca1, marca2);
+        when(marcaService.findAll()).thenReturn(marcas);
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/marca"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].nombre", is("Marca 1")))
+                .andExpect(jsonPath("$[1].nombre", is("Marca 2")));
+    }
+}
